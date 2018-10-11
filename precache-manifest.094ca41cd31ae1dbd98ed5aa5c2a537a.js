619b8960cfe9ceb79a8dd94a8767dcea0675c3a2
devron1n <devron1n@mail.ru>
2018-10-11 17:24:29 +0300

    Updates

---
+++ precache-manifest.094ca41cd31ae1dbd98ed5aa5c2a537a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0996453efe9c6032d3c",
+    "url": "/gradientapp/static/js/runtime~main.a0996453.js"
+  },
+  {
+    "revision": "47b77ce8fcb01a3cfffa",
+    "url": "/gradientapp/static/js/main.47b77ce8.chunk.js"
+  },
+  {
+    "revision": "c75aae31b2e85fc0accf",
+    "url": "/gradientapp/static/js/1.c75aae31.chunk.js"
+  },
+  {
+    "revision": "47b77ce8fcb01a3cfffa",
+    "url": "/gradientapp/static/css/main.63432f84.chunk.css"
+  },
+  {
+    "revision": "46bb007e9436b03421023efdc183eb42",
+    "url": "/gradientapp/index.html"
+  }
+];
